c2c163f73e17e0d75974d22876ea1449393bb3ed
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-09 00:16:32 +0000

    pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,8 @@ def num_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     condition = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, condition=condition)
+    file = "6-number_odd_or_even"
+    return render_template(file, n=n, condition=condition)
 
 
 if __name__ == "__main__":
