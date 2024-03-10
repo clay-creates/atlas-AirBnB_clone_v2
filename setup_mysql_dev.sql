52ed8116fbfbefd7511ac1309c31d532fa6757ed
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-10 18:12:23 -0500

    Add files via upload

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,9 @@
--- Prepare MySQL Server
-
--- Check if database hbnb_db exists
+-- hbnb_dev_db
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- If user does not exist, create it
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Apply changes
-FLUSH PRIVILEGES;
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges on the hbnb_dev_db database to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+-- Grant SELECT privilege on the performance_schema database to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';
