c1f4dd854ceb2830587aa903274ce2f9251edcd8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-12 16:23:33 +0000

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ def states_list():
     return render_template("7-states_list.html", states=models.storage.all("State").values())
 
 
-@app.teardown_appcontext()
+@app.teardown_appcontext
 def teardown_db(exception=None):
     models.storage.close()
 
