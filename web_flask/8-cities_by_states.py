4c7607aaee0499b0c8b60624de5fa8ffd48b527d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-12 15:58:41 +0000

    test with just storage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Starts a flask web app """
 from flask import Flask, render_template
-import models
+from models import storage
 
 
 app = Flask(__name__)
@@ -9,12 +9,12 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    return render_template('8-cities_by_states.py', states=models.storage.all("State").values())
+    return render_template('8-cities_by_states.py', states=storage.all("State").values())
 
 
 @app.teardown_appcontext()
 def teardown_db():
-    models.storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
